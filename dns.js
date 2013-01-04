1c210ade31772a90a56df094c8cbd525a44741c1
jackyz <jackyz.zhao@gmail.com>
2013-01-04 19:08:52 +0800

    make dns/wpad works again

--- dns.js
+++ dns.js
@@ -1,11 +1,10 @@
 var debug = require('debug')('DNS')
-    , _ = require('underscore')
     , ndns = require('native-dns');
 
 var wpad_domain = 'wpad';
 var timeout = 2000;
 
-function forwardQuery(q, cb){
+function query(q, cb){
   var self = this;
   var result = [];
   var ureq = ndns.Request({
@@ -27,7 +26,15 @@ function forwardQuery(q, cb){
   ureq.send();
 }
 
-function resolveGfw(domain, cb){
+function isGfw(domain){
+  var self = this;
+  for (var i=0; i<self.gfw.length; i++){
+    if (endsWith(domain, self.gfw[i])) return true;
+  }
+  return false;
+}
+
+function queryGfw(domain, cb){
   var self = this;
   var result = [];
   var ureq = ndns.Request({
@@ -59,11 +66,6 @@ function endsWith(str, postfix){
   return postfix == str.substr(str.length - postfix.length, postfix.length);
 }
 
-function isGfwed(domain){
-  var self = this;
-  return _(self.gfw).any(function(s){ return endsWith(domain,s); });
-}
-
 function start(config){
   // init
   var port = config.port || 53; // dns must on 53
@@ -73,74 +75,45 @@ function start(config){
     port: config.upstream.port || 53,
     type: config.upstream.protocol || 'udp'
   };
-  var self = this;
-  self.upstream = upstream;
-  self.gfw = config.gfw || ['twitter.com','facebook.com'];
-  self.wpad = config.wpad || '127.0.0.1';
+  var gfw = config.gfw || ['twitter.com','facebook.com'];
+  var wpad = config.wpad || '127.0.0.1';
   //
   var onListening = function(){
     debug("listening on %j", this.address());
   };
   var onRequest = function(req, res){
+    var self = this;
     req.ip = req._socket._remote.address;
     var q = req.question[0];
     var d = q.name;
-    var wpad_ip = self.wpad || '127.0.0.1';
     if(q['type'] == 1 && q['class'] == 1 && startsWith(d,wpad_domain)) {
       // resolve WPAD name
-      res.answer.push(ndns.A({
-        name: wpad_domain,
-        address: wpad_ip,
-        ttl: 600
-      }));
+      res.answer.push(ndns.A({name:wpad_domain, address:self.wpad, ttl:600}));
       res.send();
-      debug("WPAD %s : %j -> %j", req.ip, d, [wpad_ip]);
-    } else if(q['type'] == 1 && q['class'] == 1 && isGfwed(d)) {
-      /*
-      resolveRemote(d, function(e,r){
+      debug("%s: Query [WPAD] %j -> %j", req.ip, d, [self.wpad]);
+    } else if(q['type'] == 1 && q['class'] == 1 && isGfw.call(this, d)) {
+      queryGfw.call(this, d, function(e,r){
         if(e) {
-          debug("%s : resolveGFW %j : %j", req.ip, d, e);
-	  res.header.rcode = ndns.const.NAME_TO_RCODE.NOTFOUND;
-        } else {
-          _(r).forEach(function(ip){
-            res.answer.push(ndns.A({
-              name: d,
-              address: ip,
-              ttl: 300
-            }));
-          });
-        }
-        res.send();
-        debug("%s : resolveGFW %j -> %j", req.ip, d, r);
-      }
-      */
-      // local.resolve(d, function(e,r){
-      resolveGfw(d, function(e,r){
-        if(e) {
-          debug("GFW %s : %j : %j", req.ip, d, e);
+          debug("%s: Query [GFW] %j : %j", req.ip, d, e);
 	  res.header.rcode = ndns.consts.NAME_TO_RCODE.NOTFOUND;
         } else {
           r.forEach(function(ip){
-            res.answer.push(ndns.A({
-              name: d,
-              address: ip,
-              ttl: 300
-            }));
+            res.answer.push(ndns.A({ name: d, address: ip, ttl: 300 }));
           });
         }
         res.send();
-        debug("GFW %s : %j -> %j", req.ip, d, r);
+        debug("%s: Query [GFW] %j -> %j", req.ip, d, r);
       });
     } else {
-      forwardQuery(q, function(e,r){
+      query.call(this, q, function(e,r){
         if(e) {
-          debug("%s : forward %j : %j", req.ip, q, e);
+          debug("%s : Query %j : %j", req.ip, q, e);
 	  res.header.rcode = ndns.consts.NAME_TO_RCODE.NOTFOUND;
         } else {
           res.answer = r;
         }
         res.send();
-        debug("forward %s : %j -> %j", req.ip, q, r);
+        debug("%s: Query %j -> %j", req.ip, q, r);
       });
     }
   };
@@ -150,13 +123,20 @@ function start(config){
   var onError = function(err, buff, req, res){
     debug("error", err, buff, req, res);
   }
+
   var udpServer = ndns.createServer();
+  udpServer.upstream = upstream;
+  udpServer.gfw = gfw;
+  udpServer.wpad = wpad;
   udpServer.on('listening', onListening);
   udpServer.on('request', onRequest);
   udpServer.on('close', onClose);
   udpServer.on('error', onError);
   udpServer.serve(port, host);
   var tcpServer = ndns.createTCPServer();
+  tcpServer.upstream = upstream;
+  tcpServer.gfw = gfw;
+  tcpServer.wpad = wpad;
   tcpServer.on('listening', onListening);
   tcpServer.on('request', onRequest);
   tcpServer.on('close', onClose);
--- wpad.js
+++ wpad.js
@@ -1,8 +1,95 @@
 var debug = require('debug')('WPAD')
     , http = require('http')
     , fs = require('fs')
-    , path = require('path')
-    , _ = require('underscore');
+    , path = require('path');
+
+// underscore template
+
+  // By default, Underscore uses ERB-style template delimiters, change the
+  // following template settings to use alternative delimiters.
+  var templateSettings = {
+    evaluate    : /<%([\s\S]+?)%>/g,
+    interpolate : /<%=([\s\S]+?)%>/g,
+    escape      : /<%-([\s\S]+?)%>/g
+  };
+
+  // When customizing `templateSettings`, if you don't want to define an
+  // interpolation, evaluation or escaping regex, we need one that is
+  // guaranteed not to match.
+  var noMatch = /(.)^/;
+
+  // Certain characters need to be escaped so that they can be put into a
+  // string literal.
+  var escapes = {
+    "'":      "'",
+    '\\':     '\\',
+    '\r':     'r',
+    '\n':     'n',
+    '\t':     't',
+    '\u2028': 'u2028',
+    '\u2029': 'u2029'
+  };
+
+  var escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
+
+  // JavaScript micro-templating, similar to John Resig's implementation.
+  // Underscore templating handles arbitrary delimiters, preserves whitespace,
+  // and correctly escapes quotes within interpolated code.
+  var tmpl = function(text, data, settings) {
+    settings = settings || templateSettings;
+
+    // Combine delimiters into one regular expression via alternation.
+    var matcher = new RegExp([
+      (settings.escape || noMatch).source,
+      (settings.interpolate || noMatch).source,
+      (settings.evaluate || noMatch).source
+    ].join('|') + '|$', 'g');
+
+    // Compile the template source, escaping string literals appropriately.
+    var index = 0;
+    var source = "__p+='";
+    text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+      source += text.slice(index, offset)
+        .replace(escaper, function(match) { return '\\' + escapes[match]; });
+
+      if (escape) {
+        source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
+      }
+      if (interpolate) {
+        source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+      }
+      if (evaluate) {
+        source += "';\n" + evaluate + "\n__p+='";
+      }
+      index = offset + match.length;
+      return match;
+    });
+    source += "';\n";
+
+    // If a variable is not specified, place data values in local scope.
+    if (!settings.variable) source = 'with(obj||{}){\n' + source + '}\n';
+
+    source = "var __t,__p='',__j=Array.prototype.join," +
+      "print=function(){__p+=__j.call(arguments,'');};\n" +
+      source + "return __p;\n";
+
+    try {
+      var render = new Function(settings.variable || 'obj', source);
+    } catch (e) {
+      e.source = source;
+      throw e;
+    }
+
+    if (data) return render(data);
+    var template = function(data) {
+      return render.call(this, data);
+    };
+
+    // Provide the compiled function source as a convenience for precompilation.
+    template.source = 'function(' + (settings.variable || 'obj') + '){\n' + source + '}';
+
+    return template;
+  };
 
 var wpad_path = '/wpad.da';
 
@@ -13,7 +100,7 @@ function serveWpad(req, res, cb){
       'Content-Type': 'application/x-ns-proxy-autoconfig; charset=UTF-8',
       'Cache-Control': 'no-cache'
     });
-    res.write(self.wpad_pac);
+    res.write(self.pac);
     res.end();
     debug('%s : %s %s ok', req.ip, req.method, req.url);
     cb();
@@ -28,16 +115,14 @@ function start(config){
   // init
   var port = config.port || 80; // wpad must on 80
   var host = config.host || '0.0.0.0';
-  var pac = _.template(fs.readFileSync(path.dirname(__filename)+'/'+config.pac.template, 'utf8'), config.pac.proxy || '127.0.0.1', { interpolate : /\{\{(.+?)\}\}/g });
-  var self = this;
-  self.pac = pac;
+  var pac = tmpl(fs.readFileSync(path.dirname(__filename)+'/'+config.pac.template, 'utf8'), config.pac.proxy || {type:'PROXY',host:'127.0.0.1',port:8080});
   //
   var onListening = function(){
     debug("listening on %j", this.address());
   };
   var onRequest = function(req, res){
     req.ip = req.connection.remoteAddress;
-    serveWpad(req, res, function(e){
+    serveWpad.call(this, req, res, function(e){
       if (e) debug('%s : %s %s fail %j', req.ip, req.method, req.url, e);
     });
   };
@@ -48,6 +133,7 @@ function start(config){
     debug("error %j", err);
   };
   var server = http.createServer();
+  server.pac = pac;
   server.on('listening', onListening);
   server.on('request', onRequest);
   server.on('close', onClose);
